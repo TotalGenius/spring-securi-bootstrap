b7eb5a4ef446bb6af5ac65efa4d4c1b5b7b46937
Adel <85290866+TotalGenius@users.noreply.github.com>
2024-01-27 15:52:48 +0300

    first commit

---
+++ src/main/java/com/example/springsecuritybootstrap/SpringsecuritybootstrapApplication.java
@@ -0,0 +1,13 @@
+package com.example.springsecuritybootstrap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsecuritybootstrapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringsecuritybootstrapApplication.class, args);
+    }
+
+}
